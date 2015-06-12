475f903e87d9c371e187f818292ace5b18bae4fd
steden <1470804@qq.com>
2015-06-12 16:04:47 +0800

    提交V0.2版本

---
+++ V0/Test/Farseer.Net.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FS.Model.Members;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Farseer.Net.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Users.Data.Select(o => new {o.ID, o.LoginCount}).ToInfo();
+        }
+    }
+}
